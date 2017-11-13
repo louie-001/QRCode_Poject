abdc69b6821ea8604c99ec6c2b0b7e7dc38f6711
louie-001 <louie@xonro.com>
2017-11-13 15:34:51 +0800

    生成二维码
    使用Google zxing生成二维码

--- src/main/java/self/louie/service/QRCodeService.java
+++ src/main/java/self/louie/service/QRCodeService.java
@@ -9,8 +9,10 @@ import com.google.zxing.common.CharacterSetECI;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.Hashtable;
@@ -71,6 +73,48 @@ public class QRCodeService {
             imageFile.getParentFile().mkdirs();
         }
         ImageIO.write(bufferedImage,"png",imageFile);
+    }
+
+    /**
+     * 获取二维码图片字节数组
+     * @param content 二维码内容
+     * @param width 宽
+     * @param height 高
+     * @param margin 外边距
+     * @return 二维码对应的字节数组
+     * @throws Exception
+     */
+    public byte[] getQRCodeBytes(String content,int width,int height,int margin) throws Exception{
+        Hashtable<EncodeHintType,Object> hintTypes = new Hashtable<EncodeHintType, Object>();
+        ByteArrayOutputStream outputStream = null;
+
+        hintTypes.put(EncodeHintType.CHARACTER_SET,CharacterSetECI.UTF8);//设置编码
+        hintTypes.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);//设置容错级别
+        hintTypes.put(EncodeHintType.MARGIN,margin);//设置外边距
+
+        try {
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE,width,height,hintTypes);//获取二维码
+
+            //将二维码写入图片
+            BufferedImage bufferedImage = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < height; j++) {
+                    bufferedImage.setRGB(i,j,bitMatrix.get(i,j)? Color.RED.getRGB():Color.WHITE.getRGB());
+                }
+            }
+
+            //将图片转为字节数组
+            outputStream = new ByteArrayOutputStream();
+            ImageIO.write(bufferedImage,"png",outputStream);
+            return outputStream.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (outputStream != null){
+                outputStream.close();
+            }
+        }
     }
 
 }
--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -1,18 +1,28 @@
 import com.google.zxing.WriterException;
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import self.louie.service.QRCodeService;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
  * Created by louie on 2017-11-9.
  */
 public class TestCases {
+
     @Test
     public void testCreateDefaultQRCode() throws IOException, WriterException {
         QRCodeService service = new QRCodeService();
         service.createQRCode("hello world",300,300,"./");
+    }
 
+    @Test
+    public void testGetQRCodeBytes() throws Exception {
+        QRCodeService service = new QRCodeService();
+        byte[] bytes = service.getQRCodeBytes("https://www.runoob.com",500,500,1);
+
+        FileUtils.writeByteArrayToFile(new File("我的二维码.png"),bytes,true);
     }
 
 
